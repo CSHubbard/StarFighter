9163ce3510a7f836854b60433231de1c442ff175
Cody Hubbard <CodySpHubbard@gmail.com>
2023-03-09 01:24:38 -0800

    started the project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+  private Vector2 rawInput;
+
+  [SerializeField] private float moveSpeed = 10f;
+
+  [SerializeField] private float paddingLeft;
+  [SerializeField] private float paddingRight;
+  [SerializeField] private float paddingTop;
+  [SerializeField] private float paddingBottom;
+
+
+  Vector2 minBounds;
+  Vector2 maxBounds;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    InitBounds();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Move();
+  }
+
+  private void InitBounds()
+  {
+    Camera mainCamera = Camera.main;
+    minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+    maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+  }
+
+  private void Move()
+  {
+    Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+    Vector2 newPos = new();
+    newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+    newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingBottom);
+    transform.position = newPos;
+  }
+
+  void OnMove(InputValue value)
+  {
+    rawInput = value.Get<Vector2>();
+  }
+}
